b8cb88b14cf7aa6a8e7664072fe36f6007b629a1
sleeme2020 <sleeme2020@yandex.ru>
2023-07-09 14:29:47 +0300

    Список для ребят

---
+++ Spisok/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace Spisok
+{
+    public static class ClassCollect
+    {
+        public static List<Product> Products = new List<Product>();
+        public static List<Spisok> Spisok = new List<Spisok>();
+        public static void Add(Product product)
+        {
+            var prod =Products.FirstOrDefault(u => u == product);
+            if (prod != null) throw new Exception();
+            Products.Add(product);
+        }
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Spisok
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public List<Prod_spis> Prod_s { get;set; }
+    }
+
+    public class Prod_spis
+    {
+        public Product Product { get; set; }
+        public int value { get; set; }
+    }
+
+}
